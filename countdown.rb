409fa0daf74168734ee26d5c260465384f679635
Maika Kailani <queenmab051@gmail.com>
2019-11-08 20:52:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,19 @@ number = 10
 while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
-    puts "HAPPY NEW YEAR!"
   end
+   puts "HAPPY NEW YEAR!"
+end
+countdown
+
+def countdown_with_sleep
+number = 10
+while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
   sleep 1
+  end
+   puts "HAPPY NEW YEAR!"
 end
 countdown
 
